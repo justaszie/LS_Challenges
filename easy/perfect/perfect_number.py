cc971fbd774e4c7fd60cb8d1afbacfc0b5d4b541
Justas Zieminykas <justaxsz@gmail.com>
2024-10-16 13:25:53 +0300

    small refactoring of perfect number solution

--- easy/perfect/perfect_number.py
+++ easy/perfect/perfect_number.py
@@ -1,16 +1,17 @@
 class PerfectNumber:
 
-    @classmethod
-    def aliquot_sum(cls, number):
-        return sum([ candidate for candidate in range(1, number)
-                     if number % candidate == 0 ])
+    @staticmethod
+    def _aliquot_sum(number):
+        divisors = [ candidate for candidate in range(1, number)
+                     if number % candidate == 0 ]
+        return sum(divisors)
 
     @classmethod
     def classify(cls, number):
         if number <= 0:
             raise ValueError("Input must be a positive integer")
 
-        aliquot_sum = cls.aliquot_sum(number)
+        aliquot_sum = cls._aliquot_sum(number)
         if aliquot_sum == number:
             return 'perfect'
         elif aliquot_sum > number:
