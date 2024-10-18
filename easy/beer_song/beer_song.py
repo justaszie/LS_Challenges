15bc0cee2a5aaa12f7d0bcb2e7033cd0bcac7ce5
Justas Zieminykas <justaxsz@gmail.com>
2024-10-18 09:58:53 +0300

    Refactored the beer song challenge: single verse method uses variables to make the text dynamic.

--- easy/beer_song/beer_song.py
+++ easy/beer_song/beer_song.py
@@ -1,22 +1,20 @@
 
 class BeerSong:
     @staticmethod
-    def verse(verse_number):
-        if verse_number == 0:
-            line_1 = "No more bottles of beer on the wall, no more bottles of beer.\n"
+    def verse(bottle_count):
+        bottle_count_str = f"{bottle_count}" if bottle_count > 0 else 'No more'
+        bottle_descriptor = 'bottle' if bottle_count == 1 else 'bottles'
+
+        line_1 = f"{bottle_count_str} {bottle_descriptor} of beer on the wall, {bottle_count_str.lower()} {bottle_descriptor} of beer.\n"
+
+        if bottle_count == 0:
             line_2 = "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
         else:
-            if verse_number == 1:
-                line_1 = "1 bottle of beer on the wall, 1 bottle of beer.\n"
-                line_2 = "Take it down and pass it around, no more bottles of beer on the wall.\n"
+            take_down_descriptor = 'it' if bottle_count == 1 else 'one'
+            bottle_descriptor = 'bottle' if (bottle_count - 1) == 1 else 'bottles'
+            bottle_count_str = f"{bottle_count - 1}" if (bottle_count - 1) > 0 else 'No more'
 
-            else:
-                line_1 = f"{verse_number} bottles of beer on the wall, {verse_number} bottles of beer.\n"
-
-                if verse_number == 2:
-                    line_2 = f"Take one down and pass it around, 1 bottle of beer on the wall.\n"
-                else:
-                    line_2 = f"Take one down and pass it around, {verse_number - 1} bottles of beer on the wall.\n"
+            line_2 = f"Take {take_down_descriptor} down and pass it around, {bottle_count_str.lower()} {bottle_descriptor} of beer on the wall.\n"
 
         return line_1 + line_2
 
